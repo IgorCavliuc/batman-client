a3fd7a60b1a7193b36ccc66b53ce3fd6c9121a9e
Igor Cavliuc <cavliuc.sh@inbox.ru>
2023-06-01 21:42:13 +0300

    fix url

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
+const url  = 'https://batman-server-b1j6mxocw-igorcavliuc.vercel.app/'
 export const getAllNavigate = () => {
-  return fetch("http://localhost:3000/navigation")
+  return fetch(url+"/navigation")
     .then((res) => res.json())
     .then((res) => res);
 };
 export const getAllCategories = () => {
-  return fetch("http://localhost:3000/categories")
+  return fetch(url+"/categories")
     .then((res) => res.json())
     .then((res) => res);
 };
 export const getAllProducts = async (categoryKey) => {
   try {
-    const response = await axios.get("http://localhost:3000/products", {
+    const response = await axios.get(url+"/products", {
       params: {
         subcategory: categoryKey,
       },
@@ -26,7 +27,7 @@ export const getAllProducts = async (categoryKey) => {
 export const addPost = async (postData) => {
   try {
     const response = await axios.post(
-      "http://localhost:3000/create-post",
+      url+"/create-post",
       postData
     );
     return response;
@@ -42,7 +43,7 @@ export const getUser = async (login, password) => {
   //   password: password,
   // };
   try {
-    const response = await axios.get("http://localhost:3000/users", {
+    const response = await axios.get(url+"/users", {
       params: {
         login: login,
         password: password,
